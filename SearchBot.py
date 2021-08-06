ad237533675b7a83daab147402363d0fe07f4233
Adam Kozłowski <78177895+Adam-Koz@users.noreply.github.com>
2021-08-06 13:18:40 +0200

    Create SearchBot.py

---
+++ SearchBot.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib, time
+
+URL = 'https://www.nike.com/pl/t/skarpety-treningowe-do-kostki-everyday-cushioned-lZ03sD/SX7667-010'
+
+def CheckPrice():
+
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    title = soup.find(id = 'pdp_product_title').get_text()
+    price = soup.find_all("div", class_= "product-price css-11s12ax is--current-price")
+    Price = ''.join(price[0])[0:5]
+    ConvertedPrice = float(Price.replace(",", "."))
+    print(ConvertedPrice)
+    print(title)
+    if(ConvertedPrice < 49.99):
+        SendMail()
+
+def SendMail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    server.login("koz.adamxd@gmail.com", "hxoodtgdbquywuvw")
+    subject = "Dziwko cena skapet spadla"
+    body = "Masz link: https://www.nike.com/pl/t/skarpety-treningowe-do-kostki-everyday-cushioned-lZ03sD/SX7667-010"
+    msg = f"Subject: {subject}\n\n {body}"
+    server.sendmail(
+        'koz.adamxd@gmail.com',
+        'koz.adam1@outlook.com',
+        msg
+    )
+    print("SENDED")
+    server.quit()
+while(True):
+    CheckPrice()
+    time.sleep(10)
